2b1cd99f839394e046cf34a18b63dcbe665eddee
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-03 01:00:49 -0500

    Css is added to the about page

--- src/components/contentAbout.js
+++ src/components/contentAbout.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Image from 'gatsby-image'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
+
+const Content = styled.main`
+padding-top: 4rem;
+max-width: 1200px;
+width: 95%;
+margin: 0 auto;
+
+@media(min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+}
+
+p {
+    line-height: 2;
+}
+`
 
 const ContentAbout = () => {
 
@@ -24,11 +43,15 @@ const ContentAbout = () => {
 
     return (
         <>
-        <h2>{title}</h2>
-        <div>
+        <h2 css={css`
+                text-align: center;
+                font-size: 4rem;
+                margin-top: 4rem;
+        `}>{title}</h2>
+        <Content>
             <p>{content}</p>
             <Image fluid={image.fluid} alt="" />
-        </div>
+        </Content>
         </>
     )
 }
