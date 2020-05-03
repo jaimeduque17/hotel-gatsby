8728162398e5503685316cf5af0cc9a7a8297957
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-03 00:34:36 -0500

    Css is added to the home page

--- src/components/contentHome.js
+++ src/components/contentHome.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
+import Image from 'gatsby-image'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
+
+const HomeText = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media(min-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 2rem;
+    }
+
+    p {
+        line-height: 2;
+    }
+`
 
 const ContentHome = () => {
 
@@ -25,11 +45,15 @@ const ContentHome = () => {
 
     return (
         <>
-            <h2>{title}</h2>
-            <div>
+            <h2 css={css`
+                text-align: center;
+                font-size: 4rem;
+                margin-top: 4rem;
+            `}>{title}</h2>
+            <HomeText>
                 <p>{content}</p>
-                <img src={image.fluid.src} alt="" />
-            </div>
+                <Image fluid={image.fluid} alt="" />
+            </HomeText>
         </>
     )
 }
