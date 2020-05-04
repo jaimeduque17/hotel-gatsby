8acf4f49119ce0a8c7acc0154a2be3586a484292
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-03 20:37:31 -0500

    The room information is displayed

--- src/components/rooms.js
+++ src/components/rooms.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import Image from 'gatsby-image'
+import Layout from '../components/layout'
 
-const RoomTemplate = () => {
-    return ( <h1>Room</h1> )
+export const query = graphql`
+    query($slug: String!) {
+        allDatoCmsRoom(filter: { slug: { eq: $slug } }) {
+            nodes {
+            title
+            content
+                image {
+                    fluid(maxWidth: 1200) {
+                    ...GatsbyDatoCmsFluid
+                    }
+                }
+            }
+        }
+    }
+`
+
+const RoomTemplate = ({ data: { allDatoCmsRoom: { nodes } } }) => {
+
+    const { title, content, image } = nodes[0]
+
+    return (
+        <Layout>
+            <main>
+                <h1>{title}</h1>
+                <p>{content}</p>
+                <Image 
+                    fluid={image.fluid}
+                />
+            </main>
+        </Layout>
+    )
 }
- 
+
 export default RoomTemplate
